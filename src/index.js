b197784296f848ff053af6cd7b27998ecfe5f983
Dr Richard Haddlesey <richard.haddlesey@sky.com>
2019-04-05 15:57:09 +0100

    adding hooks
    added hooks in ResourceList

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-import "./styles.css";
-
-const App = () => {
-  const [resource, setResource] = useState("posts");
-  return (
-    <div>
-      <div>
-        <button onClick={() => setResource("posts")}>Posts</button>
-        <button onClick={() => setResource("todos")}>Todos</button>
-      </div>
-      {resource}
-    </div>
-  );
-};
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App />, document.querySelector("#root"));
